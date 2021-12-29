b6543ee49bb5d33c100f21077e53aee9b4b54c66
ali.abukahil <ali.abukahil@gmail.com>
2021-12-29 23:29:51 +0100

    Inheritance Between Classes Object.create

--- script.js
+++ script.js
@@ -469,6 +469,7 @@ console.log(tesla);
 tesla.accelerate();
  */
 
+/* 
 ////////////////////////////////////
 // Inheritance Between "Classes": ES& Classes
 class PersonC1 {
@@ -544,3 +545,37 @@ console.log(martha);
 martha.introduce();
 martha.calcAge();
 martha.greet();
+
+ */
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+// Linking two Classes
+const StudentProto = Object.create(PersonProto);
+
+// adding a method which will override the one above
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// adding another Method
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce();
+jay.calcAge();
